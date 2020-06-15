b435b4cda096b5489c04736a8b8ed2b247fe17cd
Claudia Petty <claudialove32@gmail.com>
2020-06-15 17:09:13 -0400

    very basic prompt oriented javascript app

---
+++ stalker.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your first name?");
+console.log(firstName);
+var lastName = prompt("what is your last name?");
+console.log(lastName);
+var age = prompt("what is your age?");
+console.log(age);
+
+
+console.log(`Your full name is ${firstName} ${lastName}`);
+console.log(`your age is ${age}`);
